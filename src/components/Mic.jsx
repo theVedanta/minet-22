e872ccc7a90874e22570a842bbcef3ed5a5d3f8c
Vedanta Somnathe <vedanta1412@gmail.com>
2022-07-31 18:36:22 +0530

    the

--- src/components/Mic.jsx
+++ src/components/Mic.jsx
@@ -7,41 +7,41 @@ const Mic = ({ setText, setWarning, warning, audioEnable }) => {
     filter.addWords("kabir");
     const [mic, setMic] = useState(false);
 
-    const recognize = () => {
-        window.SpeechRecognition =
-            window.SpeechRecognition || window.webkitSpeechRecognition;
-        const rec = new window.SpeechRecognition();
-        rec.interimResults = true;
+    // const recognize = () => {
+    //     window.SpeechRecognition =
+    //         window.SpeechRecognition || window.webkitSpeechRecognition;
+    //     const rec = new window.SpeechRecognition();
+    //     rec.interimResults = true;
 
-        if (!mic) {
-            rec.addEventListener("result", (e) => {
-                const spoken = Array.from(e.results)
-                    .map((result) => result[0])
-                    .map((result) => result.transcript)
-                    .join("");
+    //     if (!mic) {
+    //         rec.addEventListener("result", (e) => {
+    //             const spoken = Array.from(e.results)
+    //                 .map((result) => result[0])
+    //                 .map((result) => result.transcript)
+    //                 .join("");
 
-                const cleaned = filter.clean(spoken);
-                if (cleaned.includes("*")) {
-                    setText("");
-                    setWarning(warning + 1);
-                } else {
-                    setText(cleaned);
-                }
-            });
-            rec.start();
+    //             const cleaned = filter.clean(spoken);
+    //             if (cleaned.includes("*")) {
+    //                 setText("");
+    //                 setWarning(warning + 1);
+    //             } else {
+    //                 setText(cleaned);
+    //             }
+    //         });
+    //         rec.start();
 
-            rec.addEventListener("end", (e) => {
-                setMic(false);
-                audioEnable(false);
-                setTimeout(() => setText(""), 5000);
-            });
-        } else {
-            rec.stop();
-            setTimeout(() => setText(""), 5000);
-            setMic(false);
-            audioEnable(false);
-        }
-    };
+    //         rec.addEventListener("end", (e) => {
+    //             setMic(false);
+    //             audioEnable(false);
+    //             setTimeout(() => setText(""), 5000);
+    //         });
+    //     } else {
+    //         rec.stop();
+    //         setTimeout(() => setText(""), 5000);
+    //         setMic(false);
+    //         audioEnable(false);
+    //     }
+    // };
 
     return (
         <>
